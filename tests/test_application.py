9f6f32d9f405afc26ec6651dfb936203c9703f8b
Kiran ram <kram@lions.molloy.edu>
2025-07-07 19:03:24 -0400

    delete unit test passed

--- tests/test_application.py
+++ tests/test_application.py
@@ -85,6 +85,34 @@ def test_update_application(client, app):
 
         assert update_response.status_code == 200
         assert b'Updated' in update_response.data
+
+
+
+def test_delete_application(client, app):
+    with app.app_context():
+        test_and_login(client)  
+
+        user = User.query.filter_by(email="test@mail.com").first()
+        assert user is not None, "User should be created and logged in" 
+
+        response = client.post("/applications", json={"user_id":  str(user.id),
+                                                        "job_title": "Backend Developer",
+                                                        "company_name": "OpenAI",
+                                                        "posting": "https://openai.com/careers/backend",
+                                                        "status": "interview",
+                                                        "notes": "Submitted via referral"
+                                        })
+        assert response.status_code == 201 or response.status_code == 200
+        assert b'Application created successfully' in response.data
+
+        created_app = JobApplication.query.filter_by(user_id=str(user.id), job_title="Backend Developer").first()
+        app_id = created_app.id
+
+        delete_response = client.delete(f"/applications/{app_id}")
+
+        assert delete_response.status_code == 200
+        assert b'Application deleted successfully' in delete_response.data
+
         
 
 
